119ca4a454e76a6d5df78d72248e4f003906a896
yxni98 <60702500+yxni98@users.noreply.github.com>
2022-03-28 15:52:00 +0800

    simplify code

--- dataset.py
+++ dataset.py
@@ -2,16 +2,6 @@ from config import args
 from data_utils.data_transfer import data_transfer
 from data_utils.data_generation import data_generation
 
-read_path = './laptop_raw_data/'
-output_path = './data/raw/'
-
-data_path = './data'
-max_vocab_size = None
-min_vocab_freq = 0
-
-glove_file = './data/glove.840B.300d.txt'
-add_predefined_sentiment = './data/sentiment_dict.json'
-
-data_transfer(read_path, output_path)
+data_transfer(args.read_path, args.output_path)
 data_generation(args)
 print('data ready!')
