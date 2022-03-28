ea4608135088bac4f6a73934fe6777bc8e2d3e94
yxni98 <60702500+yxni98@users.noreply.github.com>
2022-03-28 19:12:02 +0800

    update test function

--- test.py
+++ test.py
@@ -1,8 +1,32 @@
-import yaml
 import os
 from train.test import test
+from config import args
+from model_utils.backbone import backbone_model
+from torch.utils.data import DataLoader
+from data_utils.create_dataset import create_dataset
+from train.eval import eval
 
-config = yaml.safe_load(open('config.yml'))
-mode = config['mode']
-os.environ["CUDA_VISIBLE_DEVICES"] = str(config['aspect_' + mode + '_model'][config['aspect_' + mode + '_model']['type']]['gpu'])
-test(config)
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_device
+
+def make_term_test_data():
+    data_path = args.data_path
+    test_path = os.path.join(data_path, 'processed/test.npz')
+    test_data = create_dataset(test_path, ['context', 'aspect'])
+    test_loader = DataLoader(
+        dataset=test_data,
+        batch_size=args.batch_size,
+        shuffle=False,
+        pin_memory=True
+    )
+    return test_loader
+
+def test():
+    model = backbone_model()
+    model = model.cuda()
+    model_path = os.path.join(args.data_path, 'checkpoints/recurrent_capsnet.pth')
+    model.load_state_dict(torch.load(model_path))
+    test_loader = make_term_test_data()
+    test_accuracy = eval(model, test_loader)
+    print('test:\taccuracy: %.4f' % (test_accuracy))
+
+test()
