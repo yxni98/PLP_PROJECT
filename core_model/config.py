06d79338b9c99c1c48cf45300beebab9b520cdaa
yxni98 <60702500+yxni98@users.noreply.github.com>
2022-04-10 20:03:36 +0800

    Add files via upload

--- core_model/config.py
+++ core_model/config.py
@@ -17,7 +17,7 @@ class params(object):
 		self.capsule_size = 300
 		self.bidirectional = True
 		self.dropout = 0.1
-		self.batch_size = 16
+		self.batch_size = 64
 		self.gpu_device = '8'
 		self.optimizer = 'adam'
 
