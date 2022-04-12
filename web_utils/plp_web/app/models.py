8e43411762c195f2fd46c3a21957e097fedb94ef
yxni98 <60702500+yxni98@users.noreply.github.com>
2022-04-12 16:01:39 +0800

    Add files via upload

--- web_utils/plp_web/app/models.py
+++ web_utils/plp_web/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	_name = models.CharField(max_length=30)
+	_time = models.IntegerField()
+	_amazon_term_list_1D = models.TextField(blank=True)
+	_amazon_category_list_1D = models.TextField(blank=True)
+	_amazon_sentiment_list_1D = models.TextField(blank=True)
+	_reddit_term_list_1D = models.TextField(blank=True)
+	_reddit_category_list_1D = models.TextField(blank=True)
+	_reddit_sentiment_list_1D = models.TextField(blank=True)
+
+	def get_attributes(self):
+		return (self._name, self._time, self._amazon_term_list_1D, self._amazon_category_list_1D, self._amazon_sentiment_list_1D, self.\
+			_reddit_term_list_1D, self._reddit_category_list_1D, self._reddit_sentiment_list_1D)
--- web_utils/plp_web/app/views.py
+++ web_utils/plp_web/app/views.py
@@ -1,27 +1,144 @@
 from django.shortcuts import render
 from django.utils.html import escape
 from django.http import HttpResponse
+from app.models import Product
+import time
+import os
+import shutil
+
+from amazon_search import return_amazon_reviews
+from match_reviews_reddit import return_review_from_reddit
+from Terms_cluster.Kmeans_W2V import k_Cluster
+from Aspect_Term_Extraction.updateterm import get_terms
+from core_model.sentiment_output import return_sentiment_output
+from my_wordcloud import save_wordcloud
+from my_histogram import draw_histogram
+
+def get_sorted_terms(term_list_1D):
+	term_count = {}
+	for term in term_list_1D:
+		if term not in term_count.keys():
+			term_count[term] = 1
+		else:
+			term_count[term] += 1
+	sorted_terms = sorted(term_count.items(), key=lambda x: x[1], reverse=True)
+	return sorted_terms
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def analyse_result(request):
-	product = escape(request.GET["product"])
-	platform = escape(request.GET["platform"])
-	match_product_name = 'ipad pro mini'
-	# python manage.py migrate
-	request.session["match_product_name2"] = match_product_name
-	request.session["match_product_name3"] = match_product_name
-	if platform.lower() == 'amazon':
-		return render(request, 'amazon_result1.html', {'product_name':match_product_name})
+def generate_result(platform, product, reviews, customed_category, products, timestamp):
+		
+	for i in range(len(reviews)):
+		reviews[i] = reviews[i].lower()
+	terms = get_terms(reviews)
+	sentiment_list_1D = return_sentiment_output(platform, reviews, terms)
+	term_list_1D = []
+	for _list in terms:
+		for term in _list:
+			term_list_1D.append(term)
+	if customed_category != 'customed_category' and len(customed_category)>0:
+		category_list_1D  = k_Cluster(term_list_1D, customed_category)
 	else:
-		return render(request, 'reddit_result.html', {'product_name':match_product_name})
+		category_list_1D  = k_Cluster(term_list_1D)
 
-def result_jump2(request):
-	match_product_name2 = request.session.get("match_product_name2", default="null")
-	return render(request, 'amazon_result2.html', {'product_name':match_product_name2})
+	sorted_terms = get_sorted_terms(term_list_1D)
+	top_terms = [x[0] for x in sorted_terms[:16]]
+	save_wordcloud(top_terms, product, platform) 
 
-def result_jump3(request):
-	match_product_name3 = request.session.get("match_product_name3", default="null")
-	return render(request, 'amazon_result3.html', {'product_name':match_product_name3})
+	categories = list(set(category_list_1D))
+	positive = [0]*len(categories)
+	neutral = [0]*len(categories)
+	negative = [0]*len(categories)
+	for i in range(len(category_list_1D)):
+		sentiment = sentiment_list_1D[i]
+		category = category_list_1D[i]
+		index = categories.index(category)
+		if sentiment == 0:
+			positive[index] += 1
+		elif sentiment == 1:
+			negative[index] += 1
+		elif sentiment == 2:
+			neutral[index] += 1
+	draw_histogram(positive, neutral, negative, categories, product, platform)
+
+	if len(products)== 0:
+		if platform == 'amazon':
+			Product.objects.create(_name=product, _time=timestamp, _amazon_term_list_1D='-**-'.join(term_list_1D), \
+			 _amazon_category_list_1D='-**-'.join(category_list_1D), _amazon_sentiment_list_1D='-**-'.join([str(x) for x in sentiment_list_1D]), \
+			 _reddit_term_list_1D=' ', _reddit_category_list_1D=' ', \
+			 _reddit_sentiment_list_1D=' ') 
+		else:
+			Product.objects.create(_name=product, _time=timestamp, _amazon_term_list_1D=' ', \
+			 _amazon_category_list_1D=' ', _amazon_sentiment_list_1D=' ', \
+			 _reddit_term_list_1D='-**-'.join(term_list_1D), _reddit_category_list_1D='-**-'.join(category_list_1D), \
+			 _reddit_sentiment_list_1D='-**-'.join([str(x) for x in sentiment_list_1D])) 
+	else:
+		products[0]._name = product
+		products[0]._time = timestamp
+		if platform == 'amazon':
+			products[0]._amazon_term_list_1D = '-**-'.join(term_list_1D)
+			products[0]._amazon_category_list_1D = '-**-'.join(category_list_1D)
+			products[0]._amazon_sentiment_list_1D = '-**-'.join([str(x) for x in sentiment_list_1D]) 
+		else:	
+			products[0]._reddit_term_list_1D = '-**-'.join(term_list_1D)
+			products[0]._reddit_category_list_1D = '-**-'.join(category_list_1D)
+			products[0]._reddit_sentiment_list_1D = '-**-'.join([str(x) for x in sentiment_list_1D])
+		products[0].save()
+
+	if platform == 'amazon':
+		shutil.copyfile('D:/plp_project/web_utils/plp_web/app/static/images/'+product+'_'+platform+'_wordcloud.jpg', 'D:/plp_project/web_utils/plp_web/app/static/images/1.jpg')
+		shutil.copyfile('D:/plp_project/web_utils/plp_web/app/static/images/'+product+'_'+platform+'_histogram.jpg', 'D:/plp_project/web_utils/plp_web/app/static/images/2.jpg')
+	else:
+		shutil.copyfile('D:/plp_project/web_utils/plp_web/app/static/images/'+product+'_'+platform+'_wordcloud.jpg', 'D:/plp_project/web_utils/plp_web/app/static/images/3.jpg')
+		shutil.copyfile('D:/plp_project/web_utils/plp_web/app/static/images/'+product+'_'+platform+'_histogram.jpg', 'D:/plp_project/web_utils/plp_web/app/static/images/4.jpg')
+
+def analyse_result(request):
+	try:
+		os.remove('D:/plp_project/web_utils/plp_web/app/static/images/1.jpg')
+		os.remove('D:/plp_project/web_utils/plp_web/app/static/images/2.jpg')
+		os.remove('D:/plp_project/web_utils/plp_web/app/static/images/3.jpg')
+		os.remove('D:/plp_project/web_utils/plp_web/app/static/images/4.jpg')
+	except Exception as e:
+		pass
+	
+	product = escape(request.GET["product"])
+	customed_category = escape(request.GET["customed_category"])
+
+	timestamp = int(time.time())
+	
+	match_product_name = product
+	products = Product.objects.filter(_name=match_product_name)
+	query_tag = 0
+	if len(products) > 0:
+		query_result = products[0].get_attributes()
+		last_time = query_result[1]
+		if (last_time - timestamp) <= 86400: # no more that 1 day
+			query_tag = 1
+			
+	# print(len(products))
+	# products[0].delete()
+	
+	platform = 'amazon'
+	if query_tag == 1:
+		shutil.copyfile('D:/plp_project/web_utils/plp_web/app/static/images/'+product+'_'+platform+'_wordcloud.jpg', 'D:/plp_project/web_utils/plp_web/app/static/images/1.jpg')
+		shutil.copyfile('D:/plp_project/web_utils/plp_web/app/static/images/'+product+'_'+platform+'_histogram.jpg', 'D:/plp_project/web_utils/plp_web/app/static/images/2.jpg')
+	else:
+		reviews = return_amazon_reviews(product)
+		for i in range(len(reviews)):
+			reviews[i] = reviews[i].replace('<','').replace('>','')
+		generate_result(platform, product, reviews, customed_category, products, timestamp)
+
+	platform = 'reddit'
+	if query_tag == 1:
+		shutil.copyfile('D:/plp_project/web_utils/plp_web/app/static/images/'+product+'_'+platform+'_wordcloud.jpg', 'D:/plp_project/web_utils/plp_web/app/static/images/3.jpg')
+		shutil.copyfile('D:/plp_project/web_utils/plp_web/app/static/images/'+product+'_'+platform+'_histogram.jpg', 'D:/plp_project/web_utils/plp_web/app/static/images/4.jpg')
+	else:
+		reviews = return_review_from_reddit(product)
+		for i in range(len(reviews)):
+			reviews[i] = reviews[i].replace('<','').replace('>','')
+		generate_result(platform, product, reviews, customed_category, products, timestamp)
+
+
+	return render(request, 'result.html', {'product_name':match_product_name})
